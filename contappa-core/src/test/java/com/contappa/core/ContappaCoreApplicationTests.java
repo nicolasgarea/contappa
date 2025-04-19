72ca70d0f4080c08bce25270185788d62565c6fa
Nicolas Garea <nicogareacidre@gmail.com>
2025-04-19 19:53:18 +0200

    chore: adds initial core structure (#1)

---
+++ contappa-core/src/test/java/com/contappa/core/ContappaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contappa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContappaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

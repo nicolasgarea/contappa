0066e2556f1a41ec5bedd68f1fdb7b8787f73823
nicolasgarea <nicogareacidre@gmail.com>
2025-05-05 14:25:34 +0200

    feat: add test for Table model

--- contappa-core/src/test/java/com/contappa/core/models/TableTest.java
+++ contappa-core/src/test/java/com/contappa/core/models/TableTest.java
@@ -1,6 +1,21 @@
 package com.contappa.core.models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TableTest {
+
+    @Test
+    public void testTableConstructor() {
+        Table table = new Table();
+        assertNotNull(table);
+    }
+    @Test
+    public void testTableConstructorWithParameter() {
+        Table table = new Table(5);
+        assertNotNull(table, "Is null");
+        assertEquals(5,table.getNumber());
+    }
 
 }

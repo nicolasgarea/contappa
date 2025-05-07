616c11bbe468e10d4317077c165fc071d33df0ec
Nicolas Garea <nicogareacidre@gmail.com>
2025-05-07 20:09:51 +0200

    feat: add table model  (#10)

---
+++ contappa-core/src/main/java/com/contappa/core/models/Tables.java
@@ -0,0 +1,38 @@
+package com.contappa.core.models;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tables")
+public class Tables {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "number")
+    private int number;
+
+    public Tables() {
+    }
+
+    public Tables(int number) {
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}

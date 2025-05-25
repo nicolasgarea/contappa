1950388ea9e5d785c296fec417ab44d4ddef5a6d
nicolasgarea <nicogareacidre@gmail.com>
2025-05-25 17:15:42 +0200

    feat: add BillDTO

---
+++ contappa-core/src/main/java/com/contappa/core/dto/BillDTO.java
@@ -0,0 +1,66 @@
+package com.contappa.core.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public class BillDTO {
+
+    private String id;
+    private BigDecimal amount;
+    private LocalDate date;
+    private List<ProductQuantity> products;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<ProductQuantity> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductQuantity> products) {
+        this.products = products;
+    }
+
+    public static class ProductQuantity {
+        private String productId;
+        private int quantity;
+
+        public String getProductId() {
+            return productId;
+        }
+
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}

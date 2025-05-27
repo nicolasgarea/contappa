2d40e69d0caf26d4f7c10fc0d9ae9a25721d6b17
Nicolas Garea <nicogareacidre@gmail.com>
2025-05-27 18:42:25 +0200

    feat: add ProductDTO (#24)

---
+++ contappa-core/src/main/java/com/contappa/core/dto/ProductDTO.java
@@ -0,0 +1,33 @@
+package com.contappa.core.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+    private String id;
+    private String name;
+    private BigDecimal price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}

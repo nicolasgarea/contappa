7ba1a8a1c95a402077c4872ac3d345cb1476efb5
Nicolas Garea <nicogareacidre@gmail.com>
2025-06-05 16:48:58 +0200

    feat: add mapping methods for CreateTableRequestDTO and UpdateTableRequestDTO

--- contappa-core/src/main/java/com/contappa/core/mappers/TablesMapper.java
+++ contappa-core/src/main/java/com/contappa/core/mappers/TablesMapper.java
@@ -1,6 +1,8 @@
 package com.contappa.core.mappers;
 
+import com.contappa.core.dto.CreateTableRequestDTO;
 import com.contappa.core.dto.TablesDTO;
+import com.contappa.core.dto.UpdateTableRequestDTO;
 import com.contappa.core.models.Tables;
 import org.mapstruct.Mapper;
 
@@ -10,4 +12,8 @@ public interface TablesMapper {
     TablesDTO toTablesDTO(Tables table);
 
     Tables toTables(TablesDTO tableDTO);
+
+    Tables toTables(CreateTableRequestDTO createTableRequestDTO);
+
+    Tables toTables(UpdateTableRequestDTO updateTableRequestDTO);
 }

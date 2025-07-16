0a16a3912a1fb4dae217911673a8a6d924f84b1f
Nicolas Garea <nicogareacidre@gmail.com>
2025-07-16 13:02:59 +0200

    feat: add bill controller

---
+++ contappa-core/src/main/java/com/contappa/core/controllers/BillController.java
@@ -0,0 +1,45 @@
+package com.contappa.core.controllers;
+
+import com.contappa.core.dto.BillDTO;
+import com.contappa.core.dto.CreateBillRequestDTO;
+import com.contappa.core.services.BillService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/bills")
+public class BillController {
+    private final BillService billService;
+
+    public BillController(BillService billService){
+        this.billService = billService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BillDTO> createBill(@RequestBody CreateBillRequestDTO billRequestDTO){
+        BillDTO billDTO = billService.create(billRequestDTO);
+        return new ResponseEntity<>(billDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BillDTO> getBillById(@PathVariable UUID id){
+        BillDTO billDTO = billService.findById(id);
+        return ResponseEntity.ok(billDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<BillDTO> updateBill(@PathVariable UUID id, @RequestBody BillDTO billDTO){
+        BillDTO updated = billService.update(id, billDTO);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBill(@PathVariable UUID id){
+        billService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}

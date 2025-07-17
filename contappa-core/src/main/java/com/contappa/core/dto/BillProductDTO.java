fea81a083b6be3d57ccf2f2e657c76aa3a5e8738
Nicolas Garea <nicogareacidre@gmail.com>
2025-07-17 15:12:03 +0200

    feat: add billproduct dto

---
+++ contappa-core/src/main/java/com/contappa/core/dto/BillProductDTO.java
@@ -0,0 +1,43 @@
+package com.contappa.core.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class BillProductDTO {
+    private UUID billId;
+    private UUID productId;
+    private int quantity;
+    private BigDecimal unitPrice;
+
+    public UUID getBillId() {
+        return billId;
+    }
+
+    public void setBillId(UUID billId) {
+        this.billId = billId;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}

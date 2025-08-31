b6d78b3f51b07a748248949ddbb01081e05e005c
Nicolas Garea <nicogareacidre@gmail.com>
2025-08-31 12:31:59 +0200

    feat: add UpdateBillRequestDTO

---
+++ contappa-core/src/main/java/com/contappa/core/dto/UpdateBillRequestDTO.java
@@ -0,0 +1,67 @@
+package com.contappa.core.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class UpdateBillRequestDTO {
+
+    private BigDecimal amount;
+    private LocalDate date;
+    private UUID tableId;
+    private List<CreateBillRequestDTO.ProductQuantity> products;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public UUID getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(UUID tableId) {
+        this.tableId = tableId;
+    }
+
+    public List<CreateBillRequestDTO.ProductQuantity> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<CreateBillRequestDTO.ProductQuantity> products) {
+        this.products = products;
+    }
+
+    public static class ProductQuantity {
+        private UUID productId;
+        private int quantity;
+
+        public UUID getProductId() {
+            return productId;
+        }
+
+        public void setProductId(UUID productId) {
+            this.productId = productId;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}

4a55aa06cc592436501f4ccfcbc123c49efc7eb1
Nicolas Garea <nicogareacidre@gmail.com>
2025-09-24 14:55:56 +0200

    feat: add contappa-web structure (#52)

---
+++ contappa-web/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import RootPage from '@pages/RootPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootPage />,
+  },
+])
+
+export default router
